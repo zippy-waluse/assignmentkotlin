baad238ff681f955c7bdec365f897e4e6a2a69e9
zippy-waluse <zippywaluse@gmail.com>
2024-02-23 09:21:47 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+println("hello vee")
+    modulus(num1  = 15,num2 = 4)
+    addall(10, 15, 20, 5)
+    println("I love eating")
+
+}
+
+fun printName(){
+    var name = ("Hello Vee")
+    println(name)
+}
+fun modulus(num1: Int,num2: Int): Int{
+    var result = num1 % num2
+    println(result)
+    return result
+
+}
+
+fun addall(num1:Int, num2:Int, num3:Int, num4:Int): Int{
+    var result = num1+num2+num3+num4
+    println(result)
+    return result
+}
+fun interestingFactAboutme(){
+    var name = ("I love eating")
+    println(name)
+}
+
+
+
+
+
+
+
